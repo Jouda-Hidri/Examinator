2ffb93f029d2d8fd710587e0bcc4c466a3ebeaba
Jouda Hidri <hidrijouda@gmail.com>
2017-10-11 21:41:53 +0200

    exam view added

--- src/main/java/examinator/controller/ExamController.java
+++ src/main/java/examinator/controller/ExamController.java
@@ -4,23 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import examinator.entity.Exam;
+
 @Controller
 public class ExamController {
-	
+
 	@GetMapping("/hello")
 	public String hello(Model model) {
-		
 
 		model.addAttribute("examId", "1");
 
 		return "welcome";
 	}
-	
-	@GetMapping("/exam/${examId}")
-	public String getExam(Model model) {
-		
 
-		model.addAttribute("examId", "1");
+	@GetMapping("/exam/{id}")
+	public String getExam(Model model) {
+		Exam exam = new Exam();
+		exam.setTitle("example title");
+
+		model.addAttribute("exam", exam);
 
 		return "exam";
 	}
--- src/main/java/examinator/entity/Exam.java
+++ src/main/java/examinator/entity/Exam.java
@@ -12,8 +12,18 @@ public class Exam {
 	@Id
 	@Column(name = "EXAM_ID")
 	private long id;
+	
+	private String title;
 
 	@OneToMany
 	private List<Question> questions;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 }
